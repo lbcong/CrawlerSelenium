0bc8a33c6b43856d8b78f3b557f37db6548a548f
Alex <lbcong15@gmail.com>
2017-08-05 01:26:48 +0700

    up

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT target/*.war || tar -xvjf firefox-55.0b13.tar.bz2 || tar -zxvf geckodriver-v0.18.0-linux64.tar.gz || tar -zxvf libs.tar.gz || export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/libs
+tar -xvjf firefox-55.0b13.tar.bz2 || tar -zxvf geckodriver-v0.18.0-linux64.tar.gz || tar -zxvf libs.tar.gz || export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/libs || java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
